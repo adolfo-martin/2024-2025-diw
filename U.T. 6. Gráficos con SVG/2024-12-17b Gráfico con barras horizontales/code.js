893027c25fe391ef76119aa43bc64b2be3ff72ab
adolfo-martin <adolfo.martin@iesramonarcas.es>
2025-01-09 10:05:37 +0100

    jueves

---
+++ U.T. 6. Gráficos con SVG/2024-12-17b Gráfico con barras horizontales/code.js
@@ -0,0 +1,9 @@
+const fruits = [
+    { name: 'apples', quantity: 18, color: 'red' },
+    { name: 'bananas', quantity: 9, color: 'green' },
+    { name: 'kiwis', quantity: 27, color: 'blue' },
+    { name: 'oranges', quantity: 4, color: 'yellow' },
+    { name: 'lemons', quantity: 12, color: 'magenta' },
+];
+
+const COLORS = [ 'red', 'green', 'blue', 'yellow', 'magenta', 'cyan', 'pink', 'olive', 'orange' ];
---
+++ U.T. 7. Framework Tailwind CSS/2025-01-09c Introducción con CLI/src/index.js
@@ -0,0 +1,4 @@
+document.addEventListener('DOMContentLoaded', _ => {
+    // bg-green-500
+    document.body.classList.add("bg-indigo-200");
+});
